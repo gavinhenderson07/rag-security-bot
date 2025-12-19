2493e31bfa80d83546de127d4f8510ba46aa313c
Gavin Henderson <gavinhen37@outlook.com>
2025-12-19 10:58:04 -0600

    Added chunking function

--- rag_engine.py
+++ rag_engine.py
@@ -19,4 +19,25 @@ def clean_text(content):
 raw_data = load_text('knowledge.txt')
 clean_data = clean_text(raw_data)
 
-print(clean_data[:500])
+
+def chunk_text(content, chunk_size=200, overlap=50):
+    #function to split the clean text into smaller chunks with overlap to maintain context
+
+    word_list = content.split()
+    chunks = []
+
+    #loop through word list and chunk it
+    #start at beginning and move by step size for overlap
+    start = 0
+    stop = len(word_list)
+    step = chunk_size - overlap
+
+    for i in range(start, stop, step):
+        words = word_list[i : i + chunk_size]
+        chunk = ' '.join(words)
+        chunks.append(chunk)
+    return chunks
+
+text_chunks = chunk_text(clean_data)
+print(len(text_chunks), "chunks created.")
+print(f"Sample Chunk: {text_chunks[1]}")
