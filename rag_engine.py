ba3ccbd2e764f6cc20d6285733ea2aedc4011b9a
Gavin Henderson <gavinhen37@outlook.com>
2025-12-27 13:57:05 -0600

    Created Search Index and Test Query

--- rag_engine.py
+++ rag_engine.py
@@ -1,6 +1,14 @@
+#Gavin's RAG Engine Personal Project
+#December, 2025
+
 import re
+import tensorflow as tf
+import tensorflow_hub as hub
+import pandas as pd
+import numpy as np
 
 
+#open file and read the content
 def load_text(file_path):
     with open(file_path, 'r', encoding = 'utf-8') as file:
         content = file.read()
@@ -16,6 +24,7 @@ def clean_text(content):
 
     return content
 
+#load and clean data for further processing
 raw_data = load_text('knowledge.txt')
 clean_data = clean_text(raw_data)
 
@@ -32,6 +41,8 @@ def chunk_text(content, chunk_size=200, overlap=50):
     stop = len(word_list)
     step = chunk_size - overlap
 
+    #grab words from start to chunk size
+    #combine into a single string and add to list of chunks
     for i in range(start, stop, step):
         words = word_list[i : i + chunk_size]
         chunk = ' '.join(words)
@@ -39,5 +50,47 @@ def chunk_text(content, chunk_size=200, overlap=50):
     return chunks
 
 text_chunks = chunk_text(clean_data)
-print(len(text_chunks), "chunks created.")
-print(f"Sample Chunk: {text_chunks[1]}")
+
+
+#load in and create the model from the url 
+def load_embedding_model():
+    url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    print("Loading embedding model...")
+    model = hub.load(url)
+    print("Model loaded successfully!")
+    return model
+
+model = load_embedding_model()
+
+#convert chunks from strings into vectors
+#pass strings into model to turn into vectors
+def convert_chunks(text_chunks, model):
+    vectors = model(text_chunks)
+
+    return vectors
+
+vectors = convert_chunks(text_chunks, model)
+
+#using the model, vectors, chunks, and user question
+#find the cosine similarity (ie closest related vector) using numpy
+#to closely relate the question to the answer text
+def search_index(vectors, model, text_chunks, query):
+    #model expects an input of a list
+    query_vector = model([query])
+
+    #use np to calculate inner product (dot product) with
+    #query vector against all chunk vectors
+    #add [0] to get the scores as a 1D array, which argmax() needs
+    scores = np.inner(query_vector, vectors)[0]
+
+    #find the index with the vector with the best match
+    answer_vector = np.argmax(scores)
+
+    return text_chunks[answer_vector], scores[answer_vector]
+
+test_query = "How do I deal with and dispose of ransomeware?"
+result, score = search_index(vectors, model, text_chunks, test_query)
+
+print(f"Query: {test_query}")
+print(f"Best Match Score: {score}")
+print(f"Retrieved Chunk: {result}")
